052ad0d0ea5c94d1bea56b216d97311f86717674
jayam <malviya.jayam@gmail.com>
2017-04-30 14:46:39 -0700

    tags csr matrix created

--- main.py
+++ main.py
@@ -19,6 +19,10 @@ col_array = []
 val_array = []
 row_array = []
 
+tags_col_array = []
+tags_row_array = []
+tags_val_array = []
+
 #  global vars ends here
 
 def readTestFile():
@@ -44,8 +48,8 @@ def removestopword(document):
   return text
 
 def readTrain(file):
+  print('reading file {}'.format(file))
   data_frame = pd.read_csv(file, names = ['id','title','content','tags'])
-  print('finished reading files ... ')
   data_frame['title'] = data_frame['title'].apply(lambda x : x.decode('utf-8'))
   data_frame['content'] = data_frame['content'].apply(lambda x : x.decode('utf-8'))
 
@@ -59,7 +63,8 @@ def readTrain(file):
   data_frame['tags'] = data_frame['tags'].apply(lambda x : x.split(' '))
   data_frame.drop('title', 1, inplace= True)
   data_frame.drop('content', 1, inplace= True)
-  print('finished cleaning...')
+  data_frame.drop('id', 1, inplace= True)
+  print('finished processing file : {}'.format(file))
   return data_frame
 
 def readTrainingDataSet():
@@ -68,6 +73,7 @@ def readTrainingDataSet():
   train_data_frames = []
   for f in files:
     train_data_frames.append(readTrain(f))
+  print('Data inside Dataframe for files {}'.format(files))
   return pd.concat(train_data_frames)
 
 def assign_ids(document):
@@ -91,14 +97,14 @@ def assign_tag_ids(document):
       tag_freq[w] += 1
   return
 
-def head_dict(dict, n):
-  print('============ Dicitonary ============ ')
+def head_dict(dict, n, title):
+  print('============ {} ============ '.format(title))
   for key in dict:
     if(n == 0):
       break
     print('{} , {}'.format(key, dict[key]))
     n -=1
-  print('============ / Dicitonary ============ ')
+  print('============ /{}/ ============ '.format(title))
 
 def create_feature_ids(data_frame):
   data_frame['text'].apply(lambda x: assign_ids(x))
@@ -119,6 +125,22 @@ def create_coo_data(data_frame):
       row_array.append(row_id)
       val_array.append(doc_map[w])
     row_id += 1
+
+  # creating coo data for tags
+  row_id = 0;
+  for index, row in data_frame.iterrows():
+    tag_doc = row['tags']
+    tag_doc_map = {}
+    for w in tag_doc:
+      if w not in tag_doc_map:
+        tag_doc_map[w] = 1
+      else:
+        tag_doc_map[w] += 1
+    for w in set(tag_doc):
+      tags_col_array.append(tag_dict[w])
+      tags_row_array.append(row_id)
+      tags_val_array.append(tag_doc_map[w])
+    row_id += 1
   return
 
 def create_csr_matrix(coo_cols_array,coo_rows_array,coo_vals_array, rows, cols):
@@ -130,37 +152,29 @@ def main():
   print(train_data_frame.head())
   print('No of documents, features : {}'.format(train_data_frame.shape))
   create_feature_ids(train_data_frame)
-  # print('Number of unique words : {}'.format(len(word_dict)))
-  # print('=========================Sorted =========================')
-  # print(sorted(word_freq.values(), reverse= True))
-  # print('=========================Dict=========================')
-  # print(sorted(word_dict.values(), reverse=True))
-
-  # head_dict(tag_dict, 5)
-  # head_dict(tag_freq, 5)
+  print('Number of unique words : {}'.format(len(word_dict)))
+  head_dict(tag_dict, 5,'tag Dictionary')
+  head_dict(tag_freq, 5,'tag Freq Dictionary')
 
   create_coo_data(train_data_frame)
-
-
   coo_col_array = np.asarray(col_array)
   coo_row_array = np.asarray(row_array)
   coo_val_array = np.asarray(val_array)
-
-  print('=========================Cols Array=========================')
-  print('{}'.format(coo_col_array))
-
-  print('=========================Row Array=========================')
-  print('{}'.format(coo_row_array))
-
-  print('=========================Value Array=========================')
-  print('{}'.format(coo_val_array))
-
   totalRows = train_data_frame.shape[0]
   totalFeatures = len(word_dict)
-  csr_matrix = create_csr_matrix(coo_col_array, coo_row_array, coo_val_array, totalRows, totalFeatures)
-
+  text_csr_matrix = create_csr_matrix(coo_col_array, coo_row_array, coo_val_array, totalRows, totalFeatures)
   print('=========================Csr Matrix=========================')
-  print('{}'.format(csr_matrix))
+  print('{}'.format(text_csr_matrix ))
+
+
+  tag_coo_col_array = np.asarray(tags_col_array)
+  tag_coo_row_array = np.asarray(tags_row_array)
+  tag_coo_val_array = np.asarray(tags_val_array)
+  tag_totalRows = train_data_frame.shape[0]
+  tag_totalFeatures = len(tag_dict)
+  tag_csr_matrix = create_csr_matrix(tag_coo_col_array, tag_coo_row_array, tag_coo_val_array, tag_totalRows, tag_totalFeatures)
+  print('=========================TAGS Csr Matrix=========================')
+  print('{}'.format(tag_csr_matrix))
 
   # # test = readTestFile()
   # print('=========================TEST=========================')
