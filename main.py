ec9d493e46e129327c7c6226b4464528caaaed6b
jayam <malviya.jayam@gmail.com>
2017-04-30 14:25:50 -0700

    csr matrix created

--- main.py
+++ main.py
@@ -2,7 +2,24 @@ import numpy as np
 import pandas as pd
 from nltk.corpus import stopwords
 import re
+from scipy.sparse import coo_matrix
+
 cachedStopWords = stopwords.words("english")
+# global vars here
+word_dict = {}
+word_freq = {}
+
+tag_dict = {}
+tag_freq = {}
+
+train_data_frame = None
+test_data_frame = None
+
+col_array = []
+val_array = []
+row_array = []
+
+#  global vars ends here
 
 def readTestFile():
   file = './data/test.csv'
@@ -53,8 +70,6 @@ def readTrainingDataSet():
     train_data_frames.append(readTrain(f))
   return pd.concat(train_data_frames)
 
-word_dict = {}
-word_freq = {}
 def assign_ids(document):
   doc = document.split()
   for w in doc:
@@ -66,8 +81,6 @@ def assign_ids(document):
       word_freq[w] += 1
   return
 
-tag_dict = {}
-tag_freq = {}
 def assign_tag_ids(document):
   for w in document:
     if w not in tag_dict:  # assigning ID to the words
@@ -77,11 +90,6 @@ def assign_tag_ids(document):
     else:
       tag_freq[w] += 1
   return
-
-
-train_data_frame = None
-test_data_frame = None
-
 
 def head_dict(dict, n):
   print('============ Dicitonary ============ ')
@@ -96,22 +104,63 @@ def create_feature_ids(data_frame):
   data_frame['text'].apply(lambda x: assign_ids(x))
   data_frame['tags'].apply(lambda x: assign_tag_ids(x))
 
+def create_coo_data(data_frame):
+  row_id = 0;
+  for index, row in data_frame.iterrows():
+    doc = row['text'].split()
+    doc_map = {}
+    for w in doc:
+      if w not in doc_map:
+        doc_map[w] = 1
+      else:
+        doc_map[w] += 1
+    for w in set(doc):
+      col_array.append(word_dict[w])
+      row_array.append(row_id)
+      val_array.append(doc_map[w])
+    row_id += 1
+  return
+
+def create_csr_matrix(coo_cols_array,coo_rows_array,coo_vals_array, rows, cols):
+  csr_matrix = coo_matrix((coo_vals_array,(coo_rows_array, coo_cols_array)), shape = (rows, cols)).tocsr()
+  return csr_matrix
 
 def main():
   train_data_frame = readTrainingDataSet()
   print(train_data_frame.head())
   print('No of documents, features : {}'.format(train_data_frame.shape))
   create_feature_ids(train_data_frame)
-  print('Number of unique words : {}'.format(len(word_dict)))
-  print('=========================Sorted =========================')
-  print(sorted(word_freq.values(), reverse= True))
-  print('=========================Dict=========================')
-  print(sorted(word_dict.values(), reverse=True))
+  # print('Number of unique words : {}'.format(len(word_dict)))
+  # print('=========================Sorted =========================')
+  # print(sorted(word_freq.values(), reverse= True))
+  # print('=========================Dict=========================')
+  # print(sorted(word_dict.values(), reverse=True))
 
-  head_dict(tag_dict, 5)
-  head_dict(tag_freq, 5)
+  # head_dict(tag_dict, 5)
+  # head_dict(tag_freq, 5)
+
+  create_coo_data(train_data_frame)
 
 
+  coo_col_array = np.asarray(col_array)
+  coo_row_array = np.asarray(row_array)
+  coo_val_array = np.asarray(val_array)
+
+  print('=========================Cols Array=========================')
+  print('{}'.format(coo_col_array))
+
+  print('=========================Row Array=========================')
+  print('{}'.format(coo_row_array))
+
+  print('=========================Value Array=========================')
+  print('{}'.format(coo_val_array))
+
+  totalRows = train_data_frame.shape[0]
+  totalFeatures = len(word_dict)
+  csr_matrix = create_csr_matrix(coo_col_array, coo_row_array, coo_val_array, totalRows, totalFeatures)
+
+  print('=========================Csr Matrix=========================')
+  print('{}'.format(csr_matrix))
 
   # # test = readTestFile()
   # print('=========================TEST=========================')
